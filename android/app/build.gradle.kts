925fb7663e3fbc46aafcabd98747852db29d12dd
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-04-27 19:21:37 +0200

    Changed to correct ndk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     namespace = "com.example.flutter_exam_3"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
