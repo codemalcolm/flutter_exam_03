34bb9094f5c2ac1ec4f4b8ccb44764f9719f317d
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-04-30 11:45:24 +0200

    Finished routing to ingredients page

--- lib/cuisine_page.dart
+++ lib/cuisine_page.dart
@@ -1,39 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_exam_3/ingredients_page.dart';
 
-class CuisinePage extends StatelessWidget {
-  const CuisinePage({super.key});
+class CuisinePage extends StatefulWidget {
+  const CuisinePage({
+    super.key,
+    required this.recipesFromCategory,
+    required this.categoryName,
+  });
+
+  final List<Map<String, dynamic>> recipesFromCategory;
+  final String categoryName;
+
+  @override
+  State<CuisinePage> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<CuisinePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Cuisine Page")),
-        body: Column(
-          children: [
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Go back to category page"),
-              ),
-            ),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const IngredientsPage(),
-                    ),
-                  );
-                },
-                child: Text("Go to ingredients page"),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop(context);
+            },
+            child: Icon(Icons.arrow_back),
+          ),
+          title: Text("${widget.categoryName.capitalize()} Food"),
+        ),
+        body: Center(
+          child:
+              widget.recipesFromCategory != null
+                  ? ListView.builder(
+                    itemCount: widget.recipesFromCategory.length,
+                    itemBuilder: (context, index) {
+                      final recipe = widget.recipesFromCategory[index];
+                      return ListTile(
+                        title: Text(recipe['name']),
+                        trailing: GestureDetector(
+                          behavior: HitTestBehavior.translucent,
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder:
+                                    (context) => IngredientsPage(
+                                      ingredients: recipe['ingredients'],
+                                      foodName: recipe['name'],
+                                    ),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'See Ingredients',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blueAccent,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                  : Text('Loading recipes...'),
         ),
       ),
     );
+  }
+}
+
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
   }
 }
