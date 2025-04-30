194b0fe89395c10b710ef341638a1f67ba277460
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-04-30 11:45:43 +0200

    Mapping ingredients finished

--- lib/ingredients_page.dart
+++ lib/ingredients_page.dart
@@ -1,22 +1,49 @@
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const IngredientsPage());
+class IngredientsPage extends StatefulWidget {
+  const IngredientsPage({
+    super.key,
+    required this.ingredients,
+    required this.foodName,
+  });
+
+  final List<dynamic> ingredients;
+  final String foodName;
+
+  @override
+  State<IngredientsPage> createState() => _MyAppState();
 }
 
-class IngredientsPage extends StatelessWidget {
-  const IngredientsPage({super.key});
+class _MyAppState extends State<IngredientsPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Ingredients Page"),
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop(context);
+            },
+            child: Icon(Icons.arrow_back),
+          ),
+          title: Text(widget.foodName),
         ),
         body: Center(
-          child: Text('Hello Ingredients Page!'),
+          child:
+              widget.ingredients != null
+                  ? ListView.builder(
+                    itemCount: widget.ingredients.length,
+                    itemBuilder: (context, index) {
+                      final recipe = widget.ingredients[index];
+                      return ListTile(title: Text(recipe));
+                    },
+                  )
+                  : Text('Loading recipes...'),
         ),
       ),
     );
